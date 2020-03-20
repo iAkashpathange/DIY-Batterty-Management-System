bbf8f57ed91d7cde082beb297d02cfe02503bbab
Akashpathange <akashpathange.aaa47@gmail.com>
2020-03-20 12:24:32 +0530

    implement simple SOC measurement(voltage method) to oled
    simple "brain fucking less" retard code for SOC%

--- Arduino_nano/oled_display/oled_display.ino
+++ Arduino_nano/oled_display/oled_display.ino
@@ -20,6 +20,10 @@ float R1 = 30000.0; // Value of resistor
 float R2 = 8050.0; //  Value of resistor
 int value = 0;
 
+//SOC
+float mesV = 0.0; //a variable with a decimal point to hold battery voltage
+int perC = 0;     //a variable with no decimal point to hold battery percentage
+
 void setup() 
 {
   //volatge
@@ -38,7 +42,7 @@ void setup()
 
 void loop() 
 {
-  //calculation and read the value at analog input
+  //voltage calculation
   value = analogRead(analogInput);
   vout = (value * 5.0) / 1024.0; // see text
   vin = vout / (R2/(R1+R2)); 
@@ -55,6 +59,49 @@ void loop()
   display.setCursor(0,10); 
   display.write(4);    // ASCII Symbol Ex: 4 = Diamond symbol          
   display.print("volatge = ");  display.print(vin, 4); display.print("v");
-  display.display();
+
+  //SOC
+  mesV = vin; //use voltage value
+
+  //comparision for SOC %
+  if (mesV < 3.4){                              //battery @ 3.5V or less
+   perC = 0;                                   //% = 0
+  }
+ 
+  if(mesV > 3.5  && mesV < 3.7){                 //battery @ 3.6V
+   perC = 2;
+  }
+ 
+  if (mesV > 3.6 && mesV < 3.8){                //battery @ 3.7V
+   perC = 12;
+  }
+ 
+  if (mesV > 3.7 && mesV <3.9){                 //battery @ 3.8V
+   perC = 42;
+  }
+ 
+  if (mesV > 3.8 && mesV < 4.0){                //battery @ 3.9V
+   perC = 62;
+  }
+ 
+  if (mesV > 3.9 && mesV < 4.1){                //battery @ 4.0V
+   perC = 79;
+  }
+ 
+  if (mesV > 4.0 && mesV < 4.2){                //battery @ 4.1V
+   perC = 81;
+  }
+ 
+  if (mesV > 4.19){                             //battery @ 4.2V 
+  perC = 100;                                   //%=100
+  }
+  
+  display.setTextSize(1);             
+  display.setTextColor(WHITE);        
+  display.setCursor(0,0);  
+  display.write(4);           
+  display.print("SOC = "); display.print(perC); display.print("%");
+  
+  display.display();  //print message
   delay(500); 
 }
